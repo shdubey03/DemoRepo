16cf1cb0b877859cf0929ecd11000f011d08c0c8
shdubey03 <37382489+shdubey03@users.noreply.github.com>
2020-01-07 01:06:14 +0530

    Update Add.java

--- src/Add.java
+++ src/Add.java
@@ -1,6 +1,7 @@
 
 public class Add {
 public static void main(String[] args) {
-	System.out.println("this is ne w file");
+	System.out.println("this is new file");
+	System.out.println("New statement has been added");
 }
 }
